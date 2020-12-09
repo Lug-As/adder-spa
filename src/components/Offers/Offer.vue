38e088863ad7f3d79f6ba88fd4c7a16f1d10f04a
Artyom Sokol <skaliushartem@gmail.com>
2020-12-09 18:28:04 +0300

    Fix gallery in Offer component && add 404

--- src/components/Offers/Offer.vue
+++ src/components/Offers/Offer.vue
@@ -4,12 +4,15 @@
       <v-flex>
         <v-row>
           <v-col md="9">
-            <v-card>
+            <v-card v-if="item">
               <div class="offer-image text-center">
                 <silent-box
                   :image="{src: item.src, thumbnailHeight: 400}"
-                  style="margin-bottom: -7px;"
-                ></silent-box>
+                >
+                  <template v-slot:silentbox-item="">
+                    <img class="offer-image" :src="item.src" :alt="item.title">
+                  </template>
+                </silent-box>
               </div>
               <div class="card-body">
                 <v-card-title class="text-h4">{{ item.title }}</v-card-title>
@@ -27,9 +30,10 @@
                 </v-card-actions>
               </div>
             </v-card>
+            <h2 v-else class="text-center text-h2 error--text">404 Not Found</h2>
           </v-col>
           <v-col md="3">
-            <v-card class="text-center">
+            <v-card v-if="item" class="text-center">
               <v-card-subtitle class="text-h6 text--secondary">Seller</v-card-subtitle>
               <v-avatar size="200" class="avatar">
                 <v-img :src="user.photo"></v-img>
@@ -74,7 +78,11 @@ export default {
 .card-body {
   padding: 10px;
 }
+
 .offer-image {
-  overflow: hidden;
+  max-height: 400px;
+  min-height: 50px;
+  max-width: 100%;
+  min-width: 50px;
 }
 </style>
